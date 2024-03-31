4309d0d7563155cc15d754ec25b883df07269ac2
Shivam-0103 <128032009+Shivam-0103@users.noreply.github.com>
2024-03-31 15:20:22 +0530

    Add files via upload

---
+++ python assignment-7223/Experiment 2.1.py
@@ -0,0 +1,10 @@
+#WAP to demonstrate Slicing Operations in Strings
+print("Name:shivamswaminathan\nRoll No: 2210997223")
+
+str = "shivam iyer"
+print(str[0:8])
+print(str[8:13])
+print(str[0:13])
+
+print(str[0:13:2])
+print(str[0:13:4])
---
+++ python assignment-7223/Experiment 5.2.py
@@ -0,0 +1,8 @@
+#WAP to print elements of a list[‘q’,’w’,’e’,’r’,’t’,’y’] in 3 separatelines along with element’s both indexe positive and negative
+print("Name:shivamswaminathan\nRoll No: 2210997223")
+
+list1 = ['q', 'w', 'e', 'r', 't', 'y']
+
+print(list1)
+print(list1[0],list1[1],list1[2],list1[3],list1[4],list1[5])
+print(list1[-1],list1[-2],list1[-3],list1[-4],list1[-5],list1[-6])
